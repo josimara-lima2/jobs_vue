2961ddacdaecd9e3cbd05f4ec0b46b06029d8af7
josimara-lima2 <josimara_lima88@hotmail.com>
2024-04-03 22:43:58 -0300

    criando server

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ server.use(router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}.`);
 });
